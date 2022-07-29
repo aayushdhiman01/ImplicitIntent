43a352242136c8efd94c12605d0d566446e007bf
aayushdhiman01 <50516628+aayushdhiman01@users.noreply.github.com>
2022-07-29 22:36:42 +0530

    changes

--- app/src/main/java/com/aayush/implicitintent/MainActivity.kt
+++ app/src/main/java/com/aayush/implicitintent/MainActivity.kt
@@ -24,7 +24,18 @@ class MainActivity : AppCompatActivity() {
 //        ------------------OPENING WITH DIAL---------------------------
 //        var intent=Intent(Intent.ACTION_DIAL)
 //        startActivity(intent)
-        
-
+//        ----------------OPENING EMAIL----------------------
+//
+//        val emailIntent = Intent(Intent.ACTION_SEND)
+//        emailIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+//        emailIntent.type = "vnd.android.cursor.item/email"
+//        emailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("abc@xyz.com"))
+//        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "My Email Subject")
+//        emailIntent.putExtra(Intent.EXTRA_TEXT, "My email content")
+//        startActivity(Intent.createChooser(emailIntent, "Send mail using..."))
+//        --------------------------------------------------------------------------
+//                OPENING MESSAGES APP
+        val sendIntent = Intent(Intent.ACTION_VIEW)
+        sendIntent.data = Uri.parse("sms:")
     }
 }
