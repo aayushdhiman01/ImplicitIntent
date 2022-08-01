bf1a78f8606abb1ee4febb7c7f55f160480907ea
aayushdhiman01 <50516628+aayushdhiman01@users.noreply.github.com>
2022-08-01 12:45:38 +0530

    changes

--- app/src/main/java/com/aayush/implicitintent/MainActivity.kt
+++ app/src/main/java/com/aayush/implicitintent/MainActivity.kt
@@ -1,41 +1,53 @@
 package com.aayush.implicitintent
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.aayush.implicitintent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 //        -----------SHARING URL----------------------
-//        var intent= Intent(Intent.ACTION_VIEW)
-//        intent.setData(Uri.parse("http://www.google.com"))
-//        startActivity(intent)
+        binding.url.setOnClickListener {
+            var intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse("http://www.google.com"))
+            startActivity(intent)
+        }
 //        ---------------------------------
 //        ---------SHARING TEXT WITH APPS ------------------------
-//        val intent = Intent(Intent.ACTION_SEND)
-//        intent.type = "text/plain"
-//        intent.putExtra(Intent.EXTRA_SUBJECT, "My application name")
-//        intent.putExtra(Intent.EXTRA_TEXT, "This is my text")
-//        startActivity(Intent.createChooser(intent, "choose one"))
+        binding.email.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_SUBJECT, "My application name")
+            intent.putExtra(Intent.EXTRA_TEXT, "This is my text")
+            startActivity(Intent.createChooser(intent, "choose one"))
+        }
 //        -------------------------------------------------------------
 //        ------------------OPENING WITH DIAL---------------------------
-//        var intent=Intent(Intent.ACTION_DIAL)
-//        startActivity(intent)
+        binding.dial.setOnClickListener {
+            var intent = Intent(Intent.ACTION_DIAL)
+            startActivity(intent)
+        }
 //        ----------------OPENING EMAIL----------------------
-//
-//        val emailIntent = Intent(Intent.ACTION_SEND)
-//        emailIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-//        emailIntent.type = "vnd.android.cursor.item/email"
-//        emailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("abc@xyz.com"))
-//        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "My Email Subject")
-//        emailIntent.putExtra(Intent.EXTRA_TEXT, "My email content")
-//        startActivity(Intent.createChooser(emailIntent, "Send mail using..."))
-//        --------------------------------------------------------------------------
-//                OPENING MESSAGES APP
-        val intent = Intent(Intent.ACTION_MAIN)
-        intent.addCategory(Intent.CATEGORY_APP_MESSAGING)
-        startActivity(intent)
+        binding.email.setOnClickListener {
+            val emailIntent = Intent(Intent.ACTION_SEND)
+            emailIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            emailIntent.type = "vnd.android.cursor.item/email"
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("abc@xyz.com"))
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "My Email Subject")
+            emailIntent.putExtra(Intent.EXTRA_TEXT, "My email content")
+            startActivity(Intent.createChooser(emailIntent, "Send mail using..."))
+        }
+        binding.messages.setOnClickListener {
+            val intent = Intent(Intent.ACTION_MAIN)
+            intent.addCategory(Intent.CATEGORY_APP_MESSAGING)
+            startActivity(intent)
+        }
     }
 }
